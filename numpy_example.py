1e794d1f6cf6a2791d434edd6a333d3f54d88afc
DrInnovatio <turing1723@gmail.com>
2020-12-14 19:11:10 +1000

    Dividing the array.

--- numpy_example.py
+++ numpy_example.py
@@ -24,23 +24,31 @@ array_5 = np.random.normal(0, 1, (3, 3))
 print(array_5)
 
 # Concatenate()
-array_6 = np.randint(0, 10, (1, 1))
-array_7 = np.randint(10, 20, (1, 1))
-array_8 = np.concatenate([array_6, array_7])
-print(array_8.shape)
-print(array_8)
+# array_6 = np.randint(0, 10, (1, 1))
+# array_7 = np.randint(10, 20, (1, 1))
+# array_8 = np.concatenate([array_6, array_7])
+# print(array_8.shape)
+# print(array_8)
 
 array_9 = np.array([1, 2, 3, 4, 5, 6])
-array_10 = array_9.reshape((2, 2))
+array_10 = array_9.reshape((2, 3))
 print(array_10)
 
-# 11:13
+print("$$$$$$$$$$$$$$$")
 
+array_a = np.arange(4).reshape(1, 4)
+array_b = np.arange(8).reshape(2, 4)
 
+print(array_a)
+print(array_b)
 
-# The second test
-# The second test
-# The second test
-# The second test
-# The second test
-# The second test
+array_c = np.concatenate([array_a, array_b], axis=0)
+# axis=0 means that array_b will be following array_a.
+print(array_c)
+
+# Dividing the array
+array_d = np.arange(8).reshape(2, 4)
+left, right = np.split(array_d, [2], axis=1)
+
+print(left)
+print(right)
