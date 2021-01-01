7f3bdd2554745986f4896788c5d4cfbc5c235c78
DrInnovatio <turing1723@gmail.com>
2021-01-01 11:41:37 +1000

    training the data with number pictures.

--- KNN_Algorithm_2.py
+++ KNN_Algorithm_2.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 
 img = cv2.imread('digits.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -30,3 +31,34 @@ cv2.imwrite('test_7.png', x[35, 0])
 cv2.imwrite('test_8.png', x[40, 0])
 cv2.imwrite('test_9.png', x[45, 0])
 
+FILE_NAME = 'trained.npz'
+
+
+def load_train_data(file_name):
+    with np.load(file_name) as data:
+        train = data['train']
+        train_labels = data['train_labels']
+    return train, train_labels
+
+
+def resize20(image):
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_resize = cv2.resize(gray, (20, 20))
+    plt.imshow(cv2.cvtColor(gray_resize, cv2.COLOR_BGR2RGB))
+    plt.show()
+    return gray_resize.reshape(-1, 400).astype(np.float32)
+
+
+def check(test, train, train_labels):
+    knn = cv2.ml.KNearest_create()
+    knn.train(train, cv2.ml.ROW_SAMPLE, train_labels)
+    ret, result, neighbours, dist = knn.findNearest(test, k=5)
+
+
+train, train_labels = load_train_data(FILE_NAME)
+
+for file_name in glob.glob('./test_*.png'):
+    test = resize20(file_name)
+    result = check(test, train, train_labels)
+    print(result)
