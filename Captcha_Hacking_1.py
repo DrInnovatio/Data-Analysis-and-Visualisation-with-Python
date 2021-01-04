1bdf5d5ba7fd118f67f641b2ed04644e689520ae
DrInnovatio <turing1723@gmail.com>
2021-01-04 23:04:09 +1000

    extracting colors from images

--- Captcha_Hacking_1.py
+++ Captcha_Hacking_1.py
@@ -1,3 +1,22 @@
 # collecting photo files first.
 
+BLUE = 0
+GREEN = 1
+RED = 2
+
+def get_chars(image, color):
+    other_1 = (color + 1) % 3
+    other_2 = (color + 2) % 3
+
+    c = image[:, :, other_1] == 255
+    image[c] = [0, 0, 0]
+    c = image[:, :, other_2] == 255
+    image[c] = [0, 0, 0]
+    c = image[:, :, color] < 170
+    image[c] = [0, 0, 0]
+    c = image[:, :, color] != 0
+    image[c] = [255, 255, 255]
+
+    return image
+
 
