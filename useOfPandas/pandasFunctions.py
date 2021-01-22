0060865f547ec5c9cdb479c81e5ae519053a489e
DrInnovatio <turing1723@gmail.com>
2021-01-23 08:32:38 +1000

    pandas groupby

--- useOfPandas/pandasFunctions.py
+++ useOfPandas/pandasFunctions.py
@@ -92,3 +92,10 @@ colors = pd.Series(
     index=[1, 2, 3, 5, 8]
 )
 print(colors)
+
+arrays = [['Falcon', 'Falcon', 'Parrot', 'Parrot'],
+          ['Captive', 'Wild', 'Captive', 'Wild']]
+index = pd.MultiIndex.from_arrays(arrays, names=('Animal', 'Type'))
+df = pd.DataFrame({'Max Speed': [390., 350., 30., 20.]},
+                  index=index)
+print(df)
